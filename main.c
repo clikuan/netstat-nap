df7b334c8fe35a34a4d37f95005d04af8428dffe
chen li kuan <clikuan.cs05g@g2.nctu.edu.tw>
2017-03-14 13:44:22 +0800

    parse command

--- main.c
+++ main.c
@@ -1,12 +1,33 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <getopt.h>
 
-int main(){
+int main(int argc, char *argv[]){
+
     if (setuid(0)){
-        perror("setuid");
+        fprintf(stderr, "setuid error");
         return 1;
     }
-    
+    else{
+        char * const short_options = "tu";
+        const struct option long_options[] = {
+            {  "tcp",      0,   NULL,   't'  },
+            {  "udp",   0,   NULL,   'u'  },
+            {  NULL,      0,    NULL,   0  }
+        };
+        int c;
+        while((c = getopt_long (argc, argv, short_options, long_options, NULL)) != -1) {
+            switch(c){
+                case 't':
+                    printf("tttt\n");
+                    break;
+                case 'u':
+                    printf("uuuu\n");
+                    break;
+            }
+        }
+    }
+    return 0;
 }
 
